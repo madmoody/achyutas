5abdb36253468525c6a2a453cc5057ee8beff08a
madmoody <73907801+madmoody@users.noreply.github.com>
2023-05-15 15:39:16 +0530

    ADCLM32.c
    created

--- 8051/ADCLM32.c
+++ 8051/ADCLM32.c
@@ -1,36 +1,43 @@
-#include<reg52>
+#include <reg51.h>
+#include <stdio.h>
 
 sbit rw = P1^0;
 sbit rs = P1^1;
 sbit en = P1^2;
-
-sbit ADC_prot2 =  P2;
-sbit LCD_ port0 = P0;
+sbit ADC_port2 =  P2;
+sbit LCD_port0 = P0;
 
 sbit ADC_A = P3^0;           //make enable before start  
 sbit ADC_B = P3^1;            //start the conversion  low to high rising edge
-sbit ADC_c = P3^2;        //gives status about data high on falling edge
+sbit ADC_C = P3^2;        //gives status about data high on falling edge
 sbit ALE = P3^3;
 sbit ST = P3^4;
 sbit EOC = P3^5;
 sbit OE = P3^6;
 
 
-void delay(time)
+void delay(unsigned int time)
 {
-for(int i = 0;i<time;i++);
+	unsigned int i;
+for(i = 0;i<time;i++);
 }
-Lcd_print(unsigned char data)
+
+char Lcd_print(char lcd)
 {
-	LCD_port0 = data;
+	LCD_port0 = lcd;
+
 	rw = 0;
 	rs = 1;
 	en = 1;
 	delay(1);                            
 	en = 0;
+	return lcd;
 }
-void lcd_cmd()
+//void lcd_cmd (char lcd);
+
+void lcd_cmd(char lcd)
 {
+	LCD_port0 = lcd;
 	rw = 0;
 	rs = 0;
 	en = 1;
@@ -39,7 +46,7 @@ void lcd_cmd()
 }
 void lcd_init()
 {
-lcd_cmd(0x38);
+  lcd_cmd(0x38); 
 	delay(10);
 	lcd_cmd(0x0F);
 	delay(10);
@@ -58,7 +65,7 @@ ADC_port2 = 0xFF;
 }
 int ADC_read()
 {
-int data;
+int adcdata;
 ADC_A = 0x00;
 ADC_B = 0x00;
 ADC_C = 0x00;
@@ -76,23 +83,24 @@ while(EOC == 0);
 
 OE  =1;
 delay(25);
-data = ADC_port2;
+adcdata = ADC_port2;
 OE = 0;
+return adcdata;
 }
 int main()
 {
-int value;
-char temp[100];
+int value,i = 0;
+char temp[20];
 lcd_init();
 ADC_init();
-lcd_print(1,4,"Temperature : ");
-while(1)
+	
+
+value = ADC_read();
+sprintf(temp,"%.2f",value);
+while(temp[i] != '\0')
 {
-	value = ADC_read();
-
-sprintf(temp,".2%f",value);
-lcd_print(temp);
+	Lcd_print(temp[i]);
+	i++;
 }
+return 0;
 }
-
-
